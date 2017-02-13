bc50592aedd07ce9f521789090d2d9166eab5137
milosmladenovic5 <milosml@outlook.com>
2017-02-13 22:02:51 +0100

    milan je slepac

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 var express = require('express');
 var path = require('path');
-var bodyparser = require('body-parser');
+var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var index = require('./routes/');
+var recepee = require('./routes/recepee');
+
+
+
+var app = express();
+var port = 8000;
+//view engine
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.engine('html', require('ejs').renderFile);
+
+//set static folder
+
+
+//body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/recepee', recepee);
+
+app.listen(port, function(){
+    console.log('Server started on port', +port);
+})
