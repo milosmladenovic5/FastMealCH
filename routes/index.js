487d231430b764a40cd020e53b7c244872a207e7
0508994 <m.n.milan@hotmail.com>
2017-02-14 02:17:48 +0100

    haha
    haha

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 var express  = require('express');
 var router  = express.Router();
 var mongojs = require('mongojs');
-var db = mongojs("mongodb://localhost:27017/recepees");
+var db_R = mongojs("mongodb://localhost:27017/Recepees");
+var db_I = mongojs("mongodb://localhost:27017/Ingredients");
 
 router.get('/', function(req, res, next){
   res.render('index.html');
 });
 
 router.post('/Ingredients', function(req, res, next){
-  db.recepees.find(function(err, ingredients){
+  db.Ingredients.find(function(err, ingredients){
     if(err)
     {
       res.send(err);
