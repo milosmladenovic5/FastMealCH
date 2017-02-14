77d0a23d709fc955e2a1aa0345aadd405429dcba
0508994 <m.n.milan@hotmail.com>
2017-02-14 01:05:26 +0100

    paljevina

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 var express  = require('express');
 var router  = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs("mongodb://localhost:27017/recepees");
 
 router.get('/', function(req, res, next){
   res.render('index.html');
 });
+
+router.get('/Ingredients', function(req, res, next){
+  db.recepees.find(function(err, ingredients){
+    if(err)
+    {
+      res.send(err);
+    }
+    res.json(ingredients);
+
+  })
+});
+
 
 module.exports = router;
--- routes/recepee.js
+++ routes/recepee.js
@@ -1,8 +1,11 @@
 var express  = require('express');
 var router  = express.Router();
 
+
 router.get('/recepee', function(req, res, next){
   res.render('recepee.html');
 });
+
+
 
 module.exports = router;
