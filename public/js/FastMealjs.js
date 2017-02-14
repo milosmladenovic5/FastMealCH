a934f0b562668ba21cc04585834a8343658ba70c
milosmladenovic5 <milosml@outlook.com>
2017-02-14 15:32:37 +0100

    ja sam slepac

--- public/js/FastMealjs.js
+++ public/js/FastMealjs.js
@@ -1,46 +1,77 @@
 {
- 
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        var parent  = document.getElementById('mySidenav');
+        
+        $.post("/Ingredients", {}, function(data){
+            $.each(data, function(index, value){
+                var li  = document.createElement("li");
 
-    $('#selectIngrBtn').click(function(){
-       var parent  = document.getElementById('navbar');
-       $.post("/Ingredients", {}, function(data){
-        $.each(data, function(index, value){
-            var li  = document.createElement("li");
+                var childDiv = document.createElement("div");
+                childDiv.className="col-sm-12";
+                
+                var checkDiv = document.createElement("div");
+                checkDiv.className = "checkbox";
 
-            var childDiv = document.createElement("div");
-            childDiv.className="col-sm-12";
-            
-            var checkDiv = document.createElement("div");
-            checkDiv.className = "checkbox";
+                var label  = document.createElement("label");
 
-            var label  = document.createElement("label");
+                var input  = document.createElement("input");
+                input.type = "checkbox";
+                input.id = data[index].name;
+                input.onclick = function(){
+                    checkBoxSelect(data[index].name);
+                };
+                //input.value = data[index].name;
 
-            var input  = document.createElement("input");
-            input.type = "checkbox";
-            input.id = data[index].name;
-            //input.value = data[index].name;
+                var span  = document.createElement("span");
+                span.className = "cr";
 
-            var span  = document.createElement("span");
-            span.className = "cr";
+                var i  = document.createElement("i");
+                i.classList  = "cr-icon glyphicon glyphicon-ok";
 
-            var i  = document.createElement("i");
-            i.classList  = "cr-icon glyphicon glyphicon-ok";
+                label.innerHTML = data[index].name;
 
-            label.innerHTML = data[index].name;
+                span.appendChild(i);
+                label.appendChild(input);
+                label.appendChild(span);
+                checkDiv.appendChild(label);
+                childDiv.appendChild(checkDiv);
+                li.appendChild(childDiv);
 
-            span.appendChild(i);
-            label.appendChild(input);
-            label.appendChild(span);
-            checkDiv.appendChild(label);
-            childDiv.appendChild(checkDiv);
-            li.appendChild(childDiv);
+                parent.appendChild(li);
+            });
+         });
+    }
 
-            parent.appendChild(li);
-        });
-       });
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft= "0";
+    }
 
-    });
+    function checkBoxSelect(ingrName)
+    {
+       if(document.getElementById(ingrName).checked)
+       {
+           var ingredientName = ingrName;
 
+           var parent = document.getElementById('cart');
 
+           var childLabel = document.createElement('label');
+           childLabel.innerHTML = ingrName;
+           childLabel.style.fontSize = 20;
+           childLabel.id = "label"+ingrName;
+           
+           var span =  document.createElement('span');
+           
+           //span.appendChild(childLabel);
+           parent.appendChild(childLabel);
+        }
+        else 
+        {
+            var element = document.getElementById("label"+ingrName);
+            element.parentNode.removeChild(element);
+        }
+    }
     
 }
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 var express  = require('express');
 var router  = express.Router();
 var mongojs = require('mongojs');
-var db_R = mongojs("mongodb://localhost:27017/Recepees");
-var db_I = mongojs("mongodb://localhost:27017/Ingredients");
+var db = mongojs("mongodb://localhost:27017/Recepees");
+
 
 router.get('/', function(req, res, next){
   res.render('index.html');
 });
 
 router.post('/Ingredients', function(req, res, next){
-  db.Ingredients.find(function(err, ingredients){
+  db.ingredients.find(function(err, ingredients){
     if(err)
     {
       res.send(err);
@@ -19,5 +19,15 @@ router.post('/Ingredients', function(req, res, next){
   })
 });
 
+router.get('/Recepees', function(req, res, next){
+  db.recepees.find(function(err, recepees){
+    if(err)
+    {
+      res.send(err);
+    }
+    res.json(recepees);
+
+  })
+});
 
 module.exports = router;
